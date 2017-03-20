145ff2ce201ddc1e22b0f7972869c0a2a3a44ad0
NJVC\kkirtley <kevin.kirtley@njvc.com>
2017-03-20 14:04:41 -0500

    Initial commit

---
+++ src/BAP-Core.Net/Controllers/Web/AppController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BAP_Core.Net.Services;
+using BAP_Core.Net.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BAP_Core.Net.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+            
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com")) ModelState.AddModelError("Email", "We don't send to AOL.com");
+
+            if(ModelState.IsValid)
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "Test email", model.Message);
+
+            return View();
+
+        }
+    }
+}
--- src/BAP-Core.Net/Startup.cs
+++ src/BAP-Core.Net/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BAP_Core.Net.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,32 +13,45 @@ namespace BAP_Core.Net
 {
     public class Startup
     {
+        private readonly IConfigurationRoot _config;
+        private readonly IHostingEnvironment _env;
+
         public Startup(IHostingEnvironment env)
         {
+            _env = env;
             var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
+                .SetBasePath(_env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            _config = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //singleton of service from configuration create in startup constructor
+            services.AddSingleton(_config);
             // Add framework services.
+            if (_env.IsDevelopment() || _env.IsEnvironment("test"))
+                services.AddScoped<IMailService, DebugMailService>(); // add the sendMail service
+            else
+            //Implement real email service
+            {
+            }
+            
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddConsole(_config.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
@@ -53,7 +67,7 @@ namespace BAP_Core.Net
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=App}/{action=Index}/{id?}");
             });
         }
     }
